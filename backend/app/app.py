91b58f25e881b91ba3dfabfb703afbc0ae9a03a7
JVPH <gyrospizzeria@protonmail.com>
2023-11-20 11:42:50 -0500

    Set up application factory

---
+++ backend/app/app.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+from flask_smorest import Api
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Google Trends Queries REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    api = Api(app)
+
+    
+
+    return app
