991a1d8b4cc4ae22bdd864183be056c188290ca9
JVPH <gyrospizzeria@protonmail.com>
2023-11-20 13:56:55 -0500

    Set up blueprints and jwt manager

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,24 +1,88 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+from app.db import db
+
+from app.resources.queries import blp as QueryBlueprint
+from app.resources.comments import blp as CommentBlueprint
+from app.resources.users import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Google Trends Queries REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api/swagger-ui"
-    app.config[
-        "OPENAPI_SWAGGER_UI_URL"
-    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
     
     api = Api(app)
-
     
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    
+    jwt = JWTManager(app)
+
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+            {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401
+        )
+
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+    api.register_blueprint(CommentBlueprint)
+    api.register_blueprint(QueryBlueprint)
+    api.register_blueprint(UserBlueprint)    
 
     return app
